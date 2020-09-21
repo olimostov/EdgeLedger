e08bed1435c1329900f30110fbc31ae9a205ad5b
Amalanamdas <mostovoy.alexey@gmail.com>
2020-09-21 20:22:21 +0930

    jQuery for smooth scroolling added

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ window.onscroll = function () {
     scrolled = false;
   }
 };
+
+// Smooth scrolling
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
